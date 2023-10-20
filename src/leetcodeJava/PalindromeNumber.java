c48d93a46fa33114b87644c3106ab36e76e25fed
lpandrew <lp_andrew@yahoo.com>
2023-10-20 13:53:33 +0300

    9. Palindrome Number leetcode problem

---
+++ src/leetcodeJava/PalindromeNumber.java
@@ -0,0 +1,20 @@
+package leetcodeJava;
+
+public class PalindromeNumber {
+
+    public boolean isPalindrome(int x) {
+
+        String compare = Integer.toString(x);
+        int left = 0;
+        int right = compare.length() - 1;
+
+        while (left < right) {
+            if (compare.charAt(left) != compare.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
